fa9ad7cc102536d164ee80850830dcb7f86fdec1
wastech <fataiwasiu2@gmail.com>
2023-05-12 20:01:38 +0100

    Improve structure / format of the code. :art:

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(csurf());
+  app.use(helmet());
+  app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
