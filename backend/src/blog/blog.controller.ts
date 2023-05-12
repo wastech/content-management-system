7dec7ffc90c1a0646c9a76ddb69455d7ea04e31d
wastech <fataiwasiu2@gmail.com>
2023-05-12 18:19:53 +0100

    added header comment to Blog Controller  :sparkles:

--- backend/src/blog/blog.controller.ts
+++ backend/src/blog/blog.controller.ts
@@ -35,6 +35,17 @@ export class BlogController {
     private readonly commentService: CommentService,
   ) {}
 
+  /**
+   * Creates a new blog post with the given details and image file.
+   *
+   * This method requires the user to have admin or guest role. It accepts a file object containing the image and a DTO object containing the blog post details. It returns the created blog post with a URL to the uploaded image.
+   *
+   * @param {Express.Multer.File} file - The image file to upload.
+   * @param {CreateBlogDto} createBlogDto - The DTO object containing the blog post details.
+   * @param {Request} req - The Express request object containing the user details.
+   * @returns {Promise<Blog>} - The created blog post with a URL to the uploaded image.
+   * @throws {HttpException} - If there is an error creating the blog post.
+   */
   @Post()
   @UseGuards(RolesGuard)
   @Roles(Role.Admin, Role.Guest)
@@ -67,12 +78,27 @@ export class BlogController {
     }
   }
 
+  /**
+   * This endpoint is publicly accessible and returns a paginated list of all blog posts.
+   *
+   * @param page - The page number to retrieve (default: 1)
+   * @param limit - The number of items per page (default: 10)
+   * @returns A paginated list of blog posts.
+   */
   @Public()
   @Get()
   async getAll(@Query('page') page: number, @Query('limit') limit: number) {
     return this.blogService.findAll(page, limit);
   }
 
+  /**
+   * Retrieves a blog post by ID, category, and slug.
+   * @param {string} id - The ID of the blog post.
+   * @param {string} category - The category of the blog post.
+   * @param {string} slug - The slug of the blog post.
+   * @returns {Promise<Blog>} The blog post matching the ID, category, and slug.
+   * @throws {NotFoundException} If no blog post is found.
+   */
   @Public()
   @Get(':id/:category/:slug')
   async getPostByIdCategoryAndSlug(
@@ -92,6 +118,15 @@ export class BlogController {
     return post;
   }
 
+  /**
+   * Retrieves a list of blog posts that are similar to the specified blog post.
+   *
+   * This method is accessible without authentication and takes an ID parameter from the request URL. It uses the ID to retrieve the specified blog post from the BlogService, and then calls the BlogService method findSimilarBlogs to find other blog posts that are similar to it. The method returns a list of the similar blog posts.
+   *
+   * @param {string} id - The ID of the blog post to find similar posts for.
+   * @returns {Promise<Blog[]>} - A list of blog posts that are similar to the specified blog post.
+   * @throws {NotFoundException} - If the specified blog post does not exist.
+   */
   @Public()
   @Get(':id/similar')
   async findSimilarBlogs(@Param('id') id: string): Promise<Blog[]> {
@@ -105,6 +140,14 @@ export class BlogController {
     return similarBlogs;
   }
 
+  /**
+   * Retrieves all the blogs written by the author with the specified ID.
+   *
+   * This method is marked as public and accessible to all users. It takes an author ID parameter from the request URL and uses it to query the BlogService for all blogs written by the specified author. It returns an array of Blog objects.
+   *
+   * @param {string} authorId - The ID of the author to retrieve blogs for.
+   * @returns {Promise<Blog[]>} - An array of Blog objects written by the specified author.
+   */
   @Public()
   @Get('/author/:authorId')
   async getBlogsByAuthor(@Param('authorId') authorId: string): Promise<Blog[]> {
@@ -112,6 +155,13 @@ export class BlogController {
     return blogs;
   }
 
+  /**
+   * Returns an array of blog posts that belong to the specified category.
+   * This method is accessible to all users without authentication. It takes the category name as a parameter and queries the BlogService for all blog posts that belong to that category. The method returns an array of blog posts.
+   * @param {string} categoryName - The name of the category to query.
+   * @returns {Promise<Blog[]>} - An array of blog posts that belong to the specified category.
+   * @throws {none}
+   */
   @Public()
   @Get('category/:categoryName')
   async getBlogsByCategory(
@@ -120,6 +170,14 @@ export class BlogController {
     return this.blogService.findByCategory(categoryName);
   }
 
+  /**
+   * Retrieves all tags and their count from the blog service and returns them in a formatted object.
+   *
+   * This method is decorated with @Public() to indicate that it is accessible without authentication.
+   *
+   * @returns {Object} - An object containing an array of formatted tag objects.
+   * @throws - If there is an error retrieving the tags.
+   */
   @Public()
   @Get('tags')
   async getAllTags() {
@@ -131,6 +189,11 @@ export class BlogController {
     return { tags: formattedTags };
   }
 
+  /**
+   * Retrieves all posts that have the given tag.
+   * @param tag The tag to search for.
+   * @returns An array of posts.
+   */
   @Public()
   @Get('tag/:tag')
   async getPostsByTag(@Param('tag') tag: string) {
@@ -139,6 +202,20 @@ export class BlogController {
     return { posts };
   }
 
+  /**
+   * Endpoint to update a blog post by ID.
+   *
+   * This endpoint requires the user to have admin or author role. It accepts the ID of the post,
+   * a DTO object containing the updated details of the post, and the request object containing
+   * the user details. It returns the updated blog post.
+   *
+   * @param {string} id - The ID of the blog post to update.
+   * @param {UpdateBlogDto} updatePostDto - The DTO object containing the updated details of the post.
+   * @param {any} req - The Express request object containing the user details.
+   * @returns {Promise<void>}
+   * @throws {NotFoundException} - If the post with the given ID is not found.
+   * @throws {UnauthorizedException} - If the user is not authorized to update the post.
+   */
   @Patch(':id')
   async updatePost(
     @Param('id') id: string,
@@ -167,6 +244,14 @@ export class BlogController {
     };
   }
 
+  /**
+
+* Endpoint for searching blogs by query.
+* @route GET api/blogs/query
+* @public
+* @param {string} query - The search query string.
+* @returns {Promise<Blog[]>} - The search results as an array of blogs.
+*/
   @Public()
   @Get('query')
   async search(@Query('q') query: string) {
@@ -174,6 +259,16 @@ export class BlogController {
     return results;
   }
 
+  /**
+   * Delete a blog post with the given ID. The user must have admin or author role to delete the post.
+   * The post comments will be deleted as well.
+   *
+   * @param {string} id - The ID of the blog post to delete.
+   * @param {Request} req - The Express request object containing the user details.
+   * @returns {Promise<Object>} - An object with a message indicating the success of the delete operation.
+   * @throws {NotFoundException} - If the blog post with the given ID is not found.
+   * @throws {UnauthorizedException} - If the user is not authorized to delete the post.
+   */
   @Delete(':id')
   async deletePost(@Param('id') id: string, @Req() req: any) {
     const post = await this.blogService.getPostById(id);
